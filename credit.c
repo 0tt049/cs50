b923f75e410e4a0dd1e3110e7b14fef98922b469
Otavio Coelho <otavio.gmcoelho@gmail.com>
2022-04-24 23:57:55 +0000

    automated commit by submit50

---
+++ credit.c
@@ -0,0 +1,89 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+
+void checksum(long);
+void which_card(long);
+void firstCheck(long);
+
+int main(void)
+{
+    long user_number = get_long("Number: \n");
+    firstCheck(user_number);
+}
+
+void checksum(long card_number)
+{
+    // Luhn's algorithm is utilized to get checksum
+    long y = 0;
+    long z = 0;
+    int odd_digit = 0;
+    int even_digit = 0;
+    for (int i = 15; i >= 0; i--)
+    {
+        long x = (card_number / pow(10, i));
+        z = x - y;
+        y = x * 10;
+        if (i % 2 != 0)
+        {
+            if (z >= 5)
+            {
+                odd_digit = (((z * 2) % 10) + 1) + odd_digit;
+            }
+            else
+            {
+                odd_digit = (z * 2) + odd_digit;
+            }
+        }
+        else if (i % 2 == 0)
+        {
+            even_digit = z + even_digit;
+        }
+    }
+    int sum = even_digit + odd_digit;
+    bool is_valid = false;
+    // Passing test we call for last function
+    if (sum % 10 == 0)
+    {
+        which_card(card_number);
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+}
+
+void which_card(long card_number)
+{
+    // Checks which of the credit card brands the number corresponds. We know it must be a creditcard number, this function finds out which one it is.
+    for (int i = 15; i >= 0; i--)
+    {
+        long x = (card_number / pow(10, i));
+        if ((i == 15 || i == 12) && (x == 4))
+        {
+            printf("VISA\n");
+        }
+        else if ((i == 13) && (x == 34 || x == 37))
+        {
+            printf("AMEX\n");
+        }
+        else if ((i == 14) && (x > 50 && x < 56))
+        {
+            printf("MASTERCARD\n");
+        }
+    }
+}
+
+void firstCheck(long cardnumber)
+{
+    // Checks if cardnumber is within range
+    if (((cardnumber >= (56 * (pow(10, 14)))) || (cardnumber < (4 * (pow(10, 12))))) || ((cardnumber >= (35 * (pow(10, 13))) && (cardnumber < (37 * (pow(10, 13)))))))
+    {
+        printf("INVALID\n");
+    }
+    // Calls for next function (Luhn's algo)
+    else
+    {
+        checksum(cardnumber);
+    }
+}
