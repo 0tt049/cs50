e2666cccd14fff8bc25b6ab428f57411283ec2e4
Otavio Coelho <otavio.gmcoelho@gmail.com>
2022-06-19 13:54:13 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -9,18 +9,17 @@ string plain2cypher(string plain, int key);
 int main(int argc, string argv[])
 {
     // Variable to store user keys
-    string KEY = (argv[1]);
-    int INTKEY = atoi(KEY);
     string plaintext = "";
 
-    // Check if user provided only 1 argument &&
-    if (argc != 2)
+    if (argc != 2) // to check if command line argument is not equal to 2
     {
-        printf("Please insert a valid key.\n");
+        printf("Usage: ./caesar key\n"); // print  "Usage: ./caesar key if not equal to 2"
         return 1;
     }
+
     else
     {
+        string KEY = (argv[1]);
         // Check if key is a number
         for (int i = 0, len = strlen(KEY); i < len; i++)
         {
@@ -34,31 +33,31 @@ int main(int argc, string argv[])
             }
         }
         // Check if number is positive && get plaintext.
-        if (INTKEY > 0)
+        if (atoi(KEY) > 0)
         {
             plaintext = get_string("Plaintext: ");
+            printf("ciphertext: ");
+            for (int i = 0, len = strlen(plaintext); i < len; i++)
+            {
+                if (isalpha(plaintext[i]) && islower(plaintext[i]))
+                {
+                    printf("%c", ((((plaintext[i] - 97) + atoi(KEY)) % 26) + 97));
+                }
+
+                else if (isalpha(plaintext[i]) && isupper(plaintext[i]))
+                {
+                    printf("%c", ((((plaintext[i] - 65) + atoi(KEY)) % 26) + 65));
+                }
+                // If letter is not alphabet just print as is.
+                else
+                {
+                    printf("%c", plaintext[i]);
+                }
+            }
+            printf("\n");
+            return 0;
         }
     }
 
     // Cypher function iterate over letters. If its alphabetical it shifts the letter by "KEY" amount. %26 assures value will wrap over alphabet.
-    printf("ciphertext: ");
-    for (int i = 0, len = strlen(plaintext); i < len; i++)
-    {
-        if (isalpha(plaintext[i]) && islower(plaintext[i]))
-        {
-            printf("%c", ((((plaintext[i] - 97) + INTKEY) % 26) + 97));
-        }
-
-        else if (isalpha(plaintext[i]) && isupper(plaintext[i]))
-        {
-            printf("%c", ((((plaintext[i] - 65) + INTKEY) % 26) + 65));
-        }
-        // If letter is not alphabet just print as is.
-        else
-        {
-            printf("%c", plaintext[i]);
-        }
-    }
-    printf("\n");
-    return 0;
 }
