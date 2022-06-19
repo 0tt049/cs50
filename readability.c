2f89bb75d9fb39cc454b0a6911f10d882d07bd48
Otavio Coelho <otavio.gmcoelho@gmail.com>
2022-06-19 16:16:13 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -5,26 +5,31 @@
 #include <stdlib.h>
 #include <math.h>
 
+// Function prototypes
 int count_letters(string text);
 int count_words(string text);
 int count_sentences(string text);
 
 int main(void)
 {
+    // Asks user imput
     string text = get_string("Text: ");
 
+    // Call functions
     float letter = count_letters(text);
     float word = count_words(text);
     float sentence = count_sentences(text);
 
+    // Coleman algo
     float L = (letter / word) * 100;
     float S = (sentence / word) * 100;
     float indexfloat = (0.0588 * L) - (0.296 * S) - 15.8;
     int index = round(indexfloat);
 
-    if (index < 0)
+    // Prints result
+    if (index < 1)
     {
-        printf("Before grade 0\n");
+        printf("Before grade 1\n");
     }
     else if (index > 16)
     {
@@ -36,6 +41,7 @@ int main(void)
     }
 }
 
+// Function to count letters
 int count_letters(string text)
 {
     int letterCount = 0;
@@ -49,6 +55,7 @@ int count_letters(string text)
     return letterCount;
 }
 
+// Function to count words
 int count_words(string text)
 {
     int wordCount = 1;
@@ -62,6 +69,7 @@ int count_words(string text)
     return wordCount;
 }
 
+// Function to count sentences
 int count_sentences(string text)
 {
     int sentenceCount = 0;
