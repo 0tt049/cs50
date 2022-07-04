4e184c7fca64f009a47420ebfc5d99ec93eff543
Otavio Coelho <otavio.gmcoelho@gmail.com>
2022-07-04 11:52:18 +0000

    automated commit by check50 [check50=True]

---
+++ extensions.py
@@ -0,0 +1,20 @@
+import re
+# User prompt
+filename = input("Filename: ").lower()
+
+# File name checker
+
+if filename.endswith(.gif):
+    print("image/gif")
+elif filename.endswith(.jpg) or filename.endswith(jpeg):
+    print("image/jpeg")
+elif filename.endswith(.png):
+    print("image/png")
+elif filename.endswith(.pdf):
+    print("application/pdf")
+elif filename.endswith(.txt):
+    print("text/plain")
+elif filename.endswith(.zip):
+    print("application/zip")
+else:
+    print("application/octet-stream")
