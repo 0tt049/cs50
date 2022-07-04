4d508569155d49a96b228cf0f1e3798043ac5024
Otavio Coelho <otavio.gmcoelho@gmail.com>
2022-07-04 01:58:57 +0000

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -1,5 +1,5 @@
 answer = input(
-    "What is the answer to the Great Question of Life, the Universe, and Everything? ").lower()
+    "What is the answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
 
 if answer == "42" or answer == "forty-two" or answer == "forty two":
     print(f"Yes")
