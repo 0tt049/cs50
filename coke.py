f7529364e4cc0213bcd24e870e72ca9103bfb41e
Otavio Coelho <otavio.gmcoelho@gmail.com>
2022-07-05 01:02:28 +0000

    automated commit by check50 [check50=True]

--- coke.py
+++ coke.py
@@ -3,7 +3,9 @@ amount_due = 50
 while amount_due > 0:
     print(f"Amount Due: {amount_due}")
     payment = input("Insert Coin: ")
-    amount_due = amount_due - int(payment)
+    payment = int(payment)
+    if payment == 25 or payment == 10 or payment == 5:
+        amount_due = amount_due - payment
 amount_due = abs(amount_due)
 print(f"Change Owed: {amount_due}")
 
