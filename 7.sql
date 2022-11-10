59cb0ad2e28efd146b2eaa82067055bf6d7a513a
Otavio Coelho <otavio.gmcoelho@gmail.com>
2022-11-10 21:43:10 +0000

    automated commit by check50 [check50=True]

--- 7.sql
+++ 7.sql
@@ -1,9 +1,14 @@
 -- Write a SQL query to list all movies released in 2010 and their ratings, in descending order by rating. For movies with the same rating, order them alphabetically by title.
 -- Your query should output a table with two columns, one for the title of each movie and one for the rating of each movie.
 -- Movies that do not have ratings should not be included in the result.
-   SELECT movies.title,
-          ratings.rating
-     FROM movies
-    INNER JOIN ratings ON movies.id = ratings.movie_id
- ORDER BY rating,
+   SELECT title,
+          rating
+     FROM (
+             SELECT title,
+                    id
+               FROM movies
+              WHERE YEAR = 2010
+          )
+    INNER JOIN ratings ON id = ratings.movie_id
+ ORDER BY rating DESC,
           title;
