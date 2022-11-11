194b4a0f4ee21ef87ee8fbf2604f8aa18079f5fb
Otavio Coelho <otavio.gmcoelho@gmail.com>
2022-11-11 02:30:21 +0000

    automated commit by submit50

--- 7.sql
+++ 7.sql
@@ -1,5 +1,7 @@
-SELECT AVG(energy)
-FROM songs
-WHERE artist_id =
-    (SELECT id FROM artists
-    WHERE name = "Drake");
+   SELECT AVG(energy)
+     FROM songs
+    WHERE artist_id = (
+             SELECT id
+               FROM artists
+              WHERE name = "Drake"
+          );
